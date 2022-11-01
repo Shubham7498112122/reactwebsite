79c6a057de0ca952d3a3164933b6ad7fb0621649
Shubham7498112122 <104460551+Shubham7498112122@users.noreply.github.com>
2022-11-01 21:51:52 +0530

    Update Nevbar.js

--- src/Nevbar.js
+++ src/Nevbar.js
@@ -4,10 +4,10 @@ import "./Nevbar.css";
 
 const Nevbar=()=>{
     return(<div className="Nevbar card">
-        <Link className="card" to="/" style={{textDecoration:"none",color:"black"}}>Home</Link>
-        <Link className="card" to="/about" style={{textDecoration:"none",color:"black"}}>About</Link>
-        <Link className="card" to="/courses" style={{textDecoration:"none",color:"black"}}>Courses</Link>
-        <Link className="card" to="/contact" style={{textDecoration:"none",color:"black"}}>Contact</Link>
+        <Link className="card" to="reactwebsite" style={{textDecoration:"none",color:"black"}}>Home</Link>
+        <Link className="card" to="reactwebsite/about" style={{textDecoration:"none",color:"black"}}>About</Link>
+        <Link className="card" to="reactwebsite/courses" style={{textDecoration:"none",color:"black"}}>Courses</Link>
+        <Link className="card" to="reactwebsite/contact" style={{textDecoration:"none",color:"black"}}>Contact</Link>
     </div>)
 }
 export default Nevbar;
