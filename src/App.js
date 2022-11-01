f7bc0e71ab604aa9db512fc6f9a5dd83fcbc6259
Shubham7498112122 <104460551+Shubham7498112122@users.noreply.github.com>
2022-11-01 21:47:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ import Sider from "./Sider";
 
 const App = () => {
     return (<div className="Container">
-        <BrowserRouter>
+<BrowserRouter>
         <Nevbar/>
         <Sider/>
         <Footer/>
             <Routes>
-                <Route path="/" element={<Home/>}></Route>
+                <Route path="/reactwebsite" element={<Home/>}></Route>
+                <Route path="reactwebsite/about" element={<About/>}></Route>
+                <Route path="reactwebsite/courses" element={<Courses/>}></Route>
+                <Route path="reactwebsite/contact" element={<Contact/>}></Route>
+                <Route path="*" element={<Tuddos/>}></Route>
             </Routes>
         </BrowserRouter>
     </div>
